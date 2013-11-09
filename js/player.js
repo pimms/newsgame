c835022991a5e38e7d447a72d3e3e5846fdc4b45
Joakim Stien <joakim.stien@gmail.com>
2013-11-09 14:52:24 +0100

    You can now move with the keyboard as well

--- js/player.js
+++ js/player.js
@@ -7,23 +7,73 @@ include_once(["person.js", "money.js"])
 function Player() {
 	this.person = new Person(0);
 	this.money = new Money();
-
 	this.posY = this.person.posY;
+
+	this.mouseButtonDown = false;
+	this.anyKeysDownLastFrame = false;
+	this.keysDown = [false, false, false, false];
 
 
 	this.handleEvent = function(event) {
-		if(/*event.type === gamejs.event.MOUSE_MOTION*/ event.type === gamejs.event.MOUSE_UP) {
+		if (event.type === gamejs.event.MOUSE_DOWN) {
+			this.mouseButtonDown = true;
+		} else if (event.type === gamejs.event.MOUSE_UP) {
+			this.mouseButtonDown = false;
+		} else if(event.type === gamejs.event.MOUSE_MOTION && this.mouseButtonDown) {
 			if(event.pos[1] < this.posY) {
 				this.person.moveTo(event.pos[0], event.pos[1]+35);
 			}
 			else {
 				this.person.moveTo(event.pos[0], event.pos[1]+60);
 			}
+		} else if (event.type == gamejs.event.KEY_DOWN ||
+			       event.type == gamejs.event.KEY_UP) {
+			var idx = -1;
+
+			switch (event.key) {
+				case gamejs.event.K_w:
+				case gamejs.event.K_UP:
+					idx = 0;
+					break;
+
+				case gamejs.event.K_a:
+				case gamejs.event.K_LEFT:
+					idx = 1;
+					break;
+
+				case gamejs.event.K_s:
+				case gamejs.event.K_DOWN:
+				 	idx = 2;
+				 	break;
+
+				 case gamejs.event.K_d:
+				 case gamejs.event.K_RIGHT:
+				 	idx = 3;
+				 	break;
+			}
+
+			if (idx >= 0) {
+				this.keysDown[idx] = (event.type === gamejs.event.KEY_DOWN);
+			}
 		}
 	}
 	
 
 	this.update = function(msDuration) {
+		/* Update the persons destination based on key input (I'M SO, SO SORRY) */
+		if (this.keysDown[0] || this.keysDown[1] || this.keysDown[2] || this.keysDown[3]) {
+			this.anyKeysDownLastFrame = true;
+			var goal = [this.person.posX, this.person.posY];
+			if (this.keysDown[0]) goal[1] -= 100;	// W
+			if (this.keysDown[1]) goal[0] -= 100; 	// A
+			if (this.keysDown[2]) goal[1] += 100;	// S
+			if (this.keysDown[3]) goal[0] += 100;	// D
+			this.person.moveTo(goal[0], goal[1]);
+		} else if (this.anyKeysDownLastFrame) {
+			this.anyKeysDownLastFrame = false;
+			this.person.moveTo(this.person.posX, this.person.posY);
+		}
+
 		this.person.update(msDuration);
 		this.posY = this.person.posY;
 
