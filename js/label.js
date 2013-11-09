b530a1a99958ad3129cccfa03133f9ab2a279b0a
DerMinze <gje-and@online.no>
2013-11-09 16:05:40 +0100

    Joakims funky audio files

--- js/label.js
+++ js/label.js
@@ -5,19 +5,18 @@ function Label(typeName) {
 
 
 	this.draw = function(surface) {
-		alert(this.typeName);
-		alert("YOLO");
+		//alert(this.typeName);
 		//var font = new gamejs.font.Font('20px monospace');
     	// render text - this returns a surface with the text written on it.
     	//var helloSurface = font.render('Hello World')
 		//new gamejs.font.Font('30px Sans-serif').render(this.typeName);
-		//surface.blit((new gamejs.font.Font('30px Sans-serif')).render(this.typeName), new gamejs.Rect([20,80]));
+		surface.blit((new gamejs.font.Font('30px Sans-serif')).render(this.typeName), new gamejs.Rect([20,80]));
 	}
 
 	this.update = function(msDuration) {
 		this.age += msDuration;
 		if (this.age > this.lifetime) {
-			//shoooooooooooot meeeeeeeeeee
+			alert("Stop that text!!");
 		}
 	}
 }
