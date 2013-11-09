ed38b131674ccf4bda7032810f57f52e7157f155
Joakim Stien <joakim.stien@gmail.com>
2013-11-09 08:59:50 +0100

    REALLY merged

--- js/feet.js
+++ js/feet.js
@@ -5,7 +5,6 @@ function Feet() {
 
 	}
 
-<<<<<<< HEAD
 	this.draw = function(mainSurface, position) {
 		draw.bezierCurve(mainSurface, "#7EC736", 
 			[100, 300], [400, 300], 
@@ -13,10 +12,6 @@ function Feet() {
 		draw.bezierCurve(mainSurface, "#7EC736", 
 			[100, 300], [400, 300], 
 			[110, 250], [390, 250], 4);
-=======
-	this.draw = function(mainSurface) {
-		//draw.circle(mainSurface, "#FF0000", [150, 150], 50, 10);
->>>>>>> 0be59667e90b90547896430067f3efdfa29b2cc5
 	}
 }
 
