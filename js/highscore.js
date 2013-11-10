b4955e909e2005856386efc00a7448a8d074e458
Joakim Stien <joakim.stien@gmail.com>
2013-11-10 12:21:32 +0100

    herherh

--- js/highscore.js
+++ js/highscore.js
@@ -3,5 +3,9 @@ include_once(["metrics.js"]);
 function displayHighScore(score, metrics) {
 	$("#highscore").css("visibility", "visible");
 	
-	
+	function reloadScores() {
+		$.get("http://gtl.hig.no/displayScore.php?GameID=1", function(scores) {
+			console.log(scores);
+		});	
+	}
 }
