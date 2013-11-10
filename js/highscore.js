f8067efc8f58ca313b3d281bbb45c550d3a6e77c
Joakim Stien <joakim.stien@gmail.com>
2013-11-10 14:25:36 +0100

    Improved highscore

--- js/highscore.js
+++ js/highscore.js
@@ -1,5 +1,9 @@
 include_once(["metrics.js"]);
 
+
+/* Global final score */
+var global_score = 0;
+
 function reloadScores() {
 	$.get("proxyhs.php?url=http://gtl.hig.no/displayScore.php?GameID=1", function(scores) {
 		var array = $.trim(scores).split(" ");
@@ -22,12 +26,41 @@ function reloadScores() {
 	});	
 }
 
-function submitScore(score) {
-		
+function uploadScore(username) {
+	username.replace(" ", "%20");
+	var url = "http://gtl.hig.no/logScore.php?GameID=1&User="+username;
+	url += "&Score=" + global_score;
+	url += "&hash=" + CryptoJS.MD5(username + global_score + "spicegirls");
+	
+	console.log(url);
+	
+	$.get("proxyhs.php?url=" + url, function(result) {
+		reloadScores();
+	});
 }
 
-function displayHighScore(score, metrics) {
+function submitScore() {
+	var name = $("#usernameinput").val();
+
+	rePattern = /^[a-zA-Z]{3,}$/;
+	if (rePattern.test(name)) {
+		uploadScore(name);
+		$("#highscore .gameoverlay").append('<button onClick = "restartGame()" visibility="hidden"> New game </button>');
+	} else {
+		var div = $("#usernameerror");
+		div.empty();
+		div.append('<p style="color:#FF0000">Kun bokstaver, minst 3!</p>');
+	}
+}
+
+function calculateScore(rawScore, metrics) {
+	global_score = rawScore;
+	return global_score;
+}
+
+function displayHighScore(rawScore, metrics) {
 	$("#highscore").css("visibility", "visible");
 	
-	reloadScores();
+	var score = calculateScore(rawScore, metrics);
+	$("#listwrapper").prepend("<h3>Du fikk " + score + " poeng!</h3>");
 }
