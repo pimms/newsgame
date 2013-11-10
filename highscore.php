b8ece51d5b3356e9304ec9fd9f0c7ec976969833
Joakim Stien <joakim.stien@gmail.com>
2013-11-10 10:13:15 +0100

    I didnt commit last night, wtf

---
+++ highscore.php
@@ -0,0 +1,40 @@
+<?php
+
+function getHttp($url) {
+	// curl?
+}
+
+function postScore() {
+	$name = $_POST["name"];
+	$score = $_POST["score"];
+	$hash = $_POST["hash"];
+
+	$localHash = md5($name + $score + "SUPERSECRET");
+	if ($localHash != $hash) {
+		die("haxx0r");
+	}
+
+	$url = "http://gtl.hig.no/logScore.php?User=";
+	$url .= $name;
+	$url .= "&Score=" + $score + "&GameID=1";
+
+	getHttp($url);
+}
+
+function displayScore() {
+	$scoreString = getHttp("http://gtl.hig.no/displayScore.php?GameID=1");
+
+	// Do something with it 
+}
+
+	
+
+/***** Script Execution Entry *****/
+if (isset($_POST["name"]) &&
+	isset($_POST["score"])) {
+	postScore();
+}
+
+
+
+?>
